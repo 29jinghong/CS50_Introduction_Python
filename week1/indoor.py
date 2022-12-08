5ec8bd9d55f4b6eafa296d86d2c80b937485a7b2
jinghong wang <29jinghong@gmail.com>
2022-12-07 19:37:47 -0500

    completed week1

---
+++ week1/indoor.py
@@ -0,0 +1,9 @@
+def main():
+    # ask the user for input:
+    word = input("Enter here: ")
+    # convert the word to lowercase:
+    word = word.lower()
+    # print the output back to user
+    print(f"Here is the output: {word}")
+
+main()
