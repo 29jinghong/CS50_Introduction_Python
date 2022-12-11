06f7f069b709662cbfd52011bf08302c9273c96a
jinghong wang <29jinghong@gmail.com>
2022-12-11 09:58:45 -0500

    initial commit week2

---
+++ week2/deep.py
@@ -0,0 +1,7 @@
+answer = input("what is the answer to the greates qusetion of Life, the Univers, and Everything? : ").strip().upper()
+
+if answer == "42" or answer == "FORTY-TWO" or answer == "FORTY TWO":
+    print("Yes")
+else:
+    print("No")
+    
