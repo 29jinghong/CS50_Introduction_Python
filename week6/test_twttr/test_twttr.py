155909a63b9323f13c6b86a48d8889ed30cff60a
jinghong wang <29jinghong@gmail.com>
2023-02-19 10:42:22 -0500

    refactor test to use pytest

--- week6/test_twttr/test_twttr.py
+++ week6/test_twttr/test_twttr.py
@@ -1,27 +1,32 @@
 import twttr
 def main():
-    assert shorten("number", "nmbr")
+    
+
     assert shorten("assets", "ssts")
     assert shorten("food", "fd")
     assert shorten("aeiouw", "w")
-    assert shorten("aa", "")
+    assert shorten("", "")
     assert shorten("eeeeeeeuuuuuuiiiiiiooooaaaa", "")
-    assert shorten("0a", "0")
-    print("all test case passed")
-
-
+    assert shorten("0", "0")
 
 
 def shorten(word, answer):
     checked = twttr.check(word)
 
-    if checked == answer:
-        print('"' + word + '"',"PASSED")
-        return True
-    else:
-        print('"' + word + '"',"FAILED")
-        return False
-    
+    return checked == answer
+
+def test_normal_words():
+    result = twttr.check("number")
+    assert result == "nmbr"
+
+def test_2():
+    result = twttr.check("assets")
+    assert result == "ssts"
+
+def test_3():
+    result = twttr.check("number")
+    assert result == "nmbr"
 
 if __name__ == "__main__":
     main()
+    
