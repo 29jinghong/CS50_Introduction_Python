6e8b21605f8fa2d895a533aeef01f2e6672a9e53
jinghong wang <29jinghong@gmail.com>
2023-03-02 17:18:52 -0500

    finishing test_plates

--- week6/test_plates/test_plates.py
+++ week6/test_plates/test_plates.py
@@ -1,31 +1,34 @@
 import plates
-
-def main():
-    assert check_is_valid("CS50", True)
-    assert check_is_valid("CS05", False)
-    assert check_is_valid("CS50P", True)
-    assert check_is_valid("PI3.14", False)
-    assert check_is_valid("H", False)
-    assert check_is_valid("OUTATIME", False)
-    assert check_is_valid("0", False)
-    assert check_is_valid("!", False)
-    assert check_is_valid("qwer", True)
-    assert check_is_valid("00000000000", False)
-    assert check_is_valid("loser", True)
-    assert check_is_valid("Jinghong29", False)
-    print("all test case passed")
-
-def check_is_valid(word, answer):
-    plate = plates.is_valid(word)
-    print(plate)
-    print(answer)
-    if plate == answer:
-        print('"' + word + '"',"PASSED")
-        return True
-    else:
-        print('"' + word + '"',"FAILED")
-        return False
     
 
-if __name__ == "__main__":
-    main()
+def test_normal():
+    result = True
+    assert plates.is_valid("CS50") == result
+
+    result = False
+    assert plates.is_valid("CS05") == result
+
+    result = True
+    assert plates.is_valid("CS50P") == result
+
+    result = True
+    assert plates.is_valid("loser") == result
+
+def test_with_symble():
+    result = False
+    assert plates.is_valid("PI3.14") == result
+
+def test_one_letter():
+    result = False
+    assert plates.is_valid("Z") == result
+    
+    result = False
+    assert plates.is_valid("A") == result
+
+def test_number():
+    result = False
+    assert plates.is_valid("0") == result
+
+def test_one_symble():
+    result = False
+    assert plates.is_valid("!") == result
