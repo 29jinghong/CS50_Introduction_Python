8893657df86ae6cbbe502b331785c6202a2db495
jinghong wang <29jinghong@gmail.com>
2023-03-02 17:20:21 -0500

    finishing test_twttr

--- week6/test_twttr/test_twttr.py
+++ week6/test_twttr/test_twttr.py
@@ -1,32 +1,24 @@
 import twttr
-def main():
-    
-
-    assert shorten("assets", "ssts")
-    assert shorten("food", "fd")
-    assert shorten("aeiouw", "w")
-    assert shorten("", "")
-    assert shorten("eeeeeeeuuuuuuiiiiiiooooaaaa", "")
-    assert shorten("0", "0")
-
-
-def shorten(word, answer):
-    checked = twttr.check(word)
-
-    return checked == answer
 
 def test_normal_words():
     result = twttr.check("number")
     assert result == "nmbr"
 
-def test_2():
     result = twttr.check("assets")
     assert result == "ssts"
-
-def test_3():
-    result = twttr.check("number")
-    assert result == "nmbr"
-
-if __name__ == "__main__":
-    main()
     
+
+def test_int_numbers():
+    result = twttr.check("0")
+    assert result == "0"
+
+    result = twttr.check("1234")
+    assert result == "1234"
+    
+def test_non_value():
+    result = twttr.check("")
+    assert result == ""
+    
+def test_all_vovel():
+    result = twttr.check("eeeeeeeuuuuuuiiiiiiooooaaaa")
+    assert result == ""
