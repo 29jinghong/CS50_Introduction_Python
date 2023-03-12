b103636c4a3cc02aaf3208a65e33dc8597b895ee
jinghong wang <29jinghong@gmail.com>
2023-03-11 23:47:15 -0500

    adding lines.py

--- week7/lines/lines.py
+++ week7/lines/lines.py
@@ -1,53 +1,46 @@
 import sys
 
-
 def main():
-    argc = len(sys.argv)
+    validate_input_args()
+    lines = counting_input_lines()
+    print(lines)
 
-    if argc < 2:
-        sys.exit("Too few command-line arguments")
-    elif argc > 2:
-        sys.exit("Too many command-line arguments")
-
-    if (not sys.argv[1].endswith(".py")):
-        sys.exit("Not a python file")
-
-    try:
-        with open(sys.argv[1]) as file:
-            code = 0
-
-            for line in file:
-                if is_code(line):
-                    code += 1
-
-            print(code)
-
-    except FileNotFoundError:
-        sys.exit("File does not exist")
-
-    return 0
-
-
-def is_code(line):
+def is_code(line : str):
     if line.lstrip().startswith("#") or line.lstrip() == "":
         return False
     return True
 
+def counting_input_lines():
+    try:
+        with open(sys.argv[1]) as file:
+            num = 0
+
+            for line in file:
+                if is_code(line):
+                    num += 1
+
+            return num
+
+    except FileNotFoundError:
+        sys.exit("File does not exist")
+
+def validate_input_args():
+    arg_lengt = len(sys.argv)
+    if arg_lengt < 2:
+       sys.exit("Too few command-line arguments")
+    elif arg_lengt > 2:
+       sys.exit("Too many command-line arguments")
+
+    if (not sys.argv[1].endswith(".py")):
+       sys.exit("Not a python file")
+
+    return arg_lengt
+
+#^can use mock to test the above code.
 
 if __name__ == "__main__":
     raise SystemExit(main())
+
 # this ^ runs the main function then exit 
 #so SystemExit runs the function insdie the () and then exit the program. 
-
-#def check_argc():
-#   argc = len(sys.argv)
-#   if argc < 2:
-#        sys.exit("Too few command-line arguments")
-#    elif argc > 2:
-#        sys.exit("Too many command-line arguments")
-#
-#    if (not sys.argv[1].endswith(".py")):
-#        sys.exit("Not a python file")
-#   
-#   return argc
-#is this ^ better or worse and how do you test this kind of function if its used.
+#test line 15(finding file) next test line 24(check on how to test for exception).
